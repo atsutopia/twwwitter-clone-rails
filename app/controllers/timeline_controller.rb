fcbaabd73d7adae89abedc5fe8c63ebaf056401b
川崎暑士 <kawasaki8910@gmail.com>
2018-09-26 12:51:00 +0900

    timeline

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,5 +2,8 @@ class TimelineController < ApplicationController
   def index
     @user = User.find_by(id: session[:user_id])
     redirect_to new_session_path if @user.blank?
+
+    # TODO: ユーザー仮
+    @user = User.first
   end
 end
